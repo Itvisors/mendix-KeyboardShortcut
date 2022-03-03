6fae940f19db8394dc05a031c6e83fe2f95be15a
Johan Munneke <johanmunneke94@gmail.com>
2022-03-03 16:59:49 +0100

    Setup

--- src/KeyboardShortcut.jsx
+++ src/KeyboardShortcut.jsx
@@ -4,7 +4,37 @@ import { HelloWorldSample } from "./components/HelloWorldSample";
 import "./ui/KeyboardShortcut.css";
 
 export class KeyboardShortcut extends Component {
+    constructor(props) {
+        super(props);
+        this.handleKeyDown = this.keyDown.bind(this);
+    }
+    componentDidMount() {
+        window.addEventListener("keydown", this.handleKeyDown);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("keydown", this.handleKeyDown);
+    }
+
+    keyDown(e) {
+        // if input is selected and these keys should be ignored, do nothing
+        if (e.target.localName === "input") {
+            if (this.props.disableOnInputFocus) {
+                return;
+            }
+        }
+
+        // if key combo
+        if (true) {
+            e.preventDefault();
+            if (!e.repeat) {
+                //call action
+            }
+        }
+    }
+
     render() {
-        return <HelloWorldSample sampleText={this.props.sampleText} />;
+        //not when there is popup demo
+        return <div className={this.props.className}></div>;
     }
 }
